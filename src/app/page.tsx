7a6ccd44771f7172301cdf80fc5cc4f3ba83a238
Duarda19 <130762202+Duarda19@users.noreply.github.com>
2025-05-15 12:33:43 +0000

    pratica 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 const Titulo = () => (
 	<h1 className="text-2xl font-bold mb-1">React - Conceitos básicos</h1>
@@ -25,19 +25,23 @@ interface TarefaProps {
 }
 
 const Tarefa: React.FC<TarefaProps> = ({ titulo, concluido }) => {
+	const [estaConcluido, setEstaConcluido] = useState(concluido);
+
 	const classe = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
-		concluido
+		estaConcluido
 			? "bg-gray-800 hover:border-gray-800"
 			: "bg-gray-400 hover:border-gray-400"
 	}`;
 
+	const escutarClique = () => {
+		console.log(`A tarefa '${titulo}' foi clicada!`);
+		setEstaConcluido(!estaConcluido);
+	};
+
 	return (
-		<div
-			className={classe}
-			onClick={() => console.log(`A tarefa '${titulo}' foi clicada!`)}
-		>
+		<div className={classe} onClick={() => escutarClique()}>
 			<h3 className="text-xl font-bold">{titulo}</h3>
-			<p className="text-sm">{concluido ? "Concluída" : "Pendente"}</p>
+			<p className="text-sm">{estaConcluido ? "Concluída" : "Pendente"}</p>
 		</div>
 	);
 };
